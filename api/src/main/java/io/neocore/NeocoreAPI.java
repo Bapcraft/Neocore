87482ad9d5ec980fef0957e876dc2088cbe98dc8
treyzania <treyzania@gmail.com>
2016-06-17 00:45:14 -0400

    api: Set up Maven classes and main API class.

---
+++ api/src/main/java/io/neocore/NeocoreAPI.java
@@ -0,0 +1,12 @@
+package io.neocore;
+
+/**
+ * Main class for the Neocore API.
+ * 
+ * @author Treyzania
+ */
+public class NeocoreAPI {
+
+	// Nothing yet to see here.  Just something to let Git make these directories.
+	
+}
