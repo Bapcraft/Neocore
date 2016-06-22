5c3ed8f0bde38d0bc8d79a7a5aba11985eb47ee8
treyzania <Treyzania@gmail.com>
2016-06-22 18:44:42 -0400

    platform-bukkit: Set up gitignore, POM, and main plugin files.

---
+++ platform-bukkit/src/main/java/io/neocore/NeocoreBukkitPlugin.java
@@ -0,0 +1,28 @@
+package io.neocore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import io.neocore.host.HostPlugin;
+import io.neocore.host.HostServiceProvider;
+
+public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public List<HostServiceProvider> getServices() {
+		return new ArrayList<>();
+	}
+	
+}
