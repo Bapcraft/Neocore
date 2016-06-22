54f2e9d6f1968c92f07041e5a62fcd2d033f85f7
treyzania <Treyzania@gmail.com>
2016-06-22 13:05:02 -0400

    common: Updated NeocoreImpl to conform to the newest API spec.

--- common/src/main/java/io/neocore/NeocoreImpl.java
+++ common/src/main/java/io/neocore/NeocoreImpl.java
@@ -1,9 +1,26 @@
 package io.neocore;
 
+import java.util.UUID;
+
+import io.neocore.database.DatabaseController;
+import io.neocore.host.HostPlugin;
+import io.neocore.player.NeoPlayer;
+
 public class NeocoreImpl implements Neocore {
 
-	public NeocorePlugin getPlugin() {
+	public HostPlugin getHost() {
 		return null; // TODO Set up system to set plugin instance.
+	}
+
+	@Override
+	public DatabaseController getDatabase() {
+		return null;
+	}
+
+	@Override
+	public NeoPlayer getPlayer(UUID uuid) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
