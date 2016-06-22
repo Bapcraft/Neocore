c8cb39ec15a29059c3510b0d668d56ee414a8c7f
treyzania <treyzania@gmail.com>
2016-06-22 00:22:20 -0400

    api: Changed downstream servers to use the acutal socet instead of the address.

--- api/src/main/java/io/neocore/host/proxy/DownstreamServer.java
+++ api/src/main/java/io/neocore/host/proxy/DownstreamServer.java
@@ -1,10 +1,10 @@
 package io.neocore.host.proxy;
 
-import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public interface DownstreamServer {
 	
 	public String getName();
-	public InetAddress getAddress();
+	public InetSocketAddress getAddress();
 	
 }
