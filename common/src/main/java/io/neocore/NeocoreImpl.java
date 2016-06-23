9bed7f19abfbfa4f963b2d6c75383d05a6984a92
treyzania <Treyzania@gmail.com>
2016-06-23 02:43:33 -0400

    common: Beginnings of PlayerManager and service registration.

--- common/src/main/java/io/neocore/NeocoreImpl.java
+++ common/src/main/java/io/neocore/NeocoreImpl.java
@@ -1,26 +1,55 @@
 package io.neocore;
 
+import java.util.Map;
 import java.util.UUID;
 
+import io.neocore.Neocore;
 import io.neocore.database.DatabaseController;
 import io.neocore.host.HostPlugin;
 import io.neocore.player.NeoPlayer;
+import io.neocore.player.PlayerManager;
 
 public class NeocoreImpl implements Neocore {
-
+	
+	private final HostPlugin host;
+	
+	private PlayerManager playerManager;
+	private Map<ServiceType, ServiceProvider> services;
+	
+	public NeocoreImpl(HostPlugin host) {
+		this.host = host;
+	}
+	
 	public HostPlugin getHost() {
-		return null; // TODO Set up system to set plugin instance.
+		return this.host;
 	}
 
 	@Override
 	public DatabaseController getDatabase() {
-		return null;
+		return null; // TODO
 	}
 
 	@Override
 	public NeoPlayer getPlayer(UUID uuid) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.playerManager.getPlayer(uuid);
 	}
-
+	
+	@Override
+	public void registerServiceProvider(ServiceType type, ServiceProvider prov) {
+		
+		Class<? extends ServiceProvider> typeClazz = type.getClassType();
+		Class<? extends ServiceProvider> clazz = prov.getClass();
+		if (!typeClazz.isAssignableFrom(clazz)) throw new ClassCastException("The class " + clazz + " is not an instance of " + typeClazz + "!");
+		
+		if (this.services.containsKey(type)) throw new IllegalStateException("A provider already exists for type " + type.getName() + " when registering " + prov + "!");
+		this.services.put(type, prov);
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T extends ServiceProvider> T getServiceProvider(ServiceType serviceType) {
+		return (T) this.services.get(serviceType);
+	}
+	
 }
---
+++ common/src/main/java/io/neocore/player/PlayerManager.java
@@ -0,0 +1,36 @@
+package io.neocore.player;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
+import io.neocore.player.NeoPlayer;
+
+public class PlayerManager {
+	
+	public Set<NeoPlayer> playerCache = new TreeSet<>();
+	
+	public NeoPlayer getPlayer(UUID uuid) {
+		
+		// Try to find the player if it's there already...
+		for (NeoPlayer np : this.playerCache) {
+			if (np.getUniqueId().equals(uuid)) return np;
+		}
+		
+		// Otherwise assemble a new one.
+		return this.assemblePlayer(uuid);
+		
+	}
+	
+	public NeoPlayer assemblePlayer(UUID uuid) {
+		
+		NeoPlayer np = new NeoPlayer(uuid);
+		
+		// TODO
+		
+		this.playerCache.add(np);
+		return np;
+		
+	}
+	
+}
