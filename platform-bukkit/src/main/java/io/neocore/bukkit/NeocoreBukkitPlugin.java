99d019c0cdb2905c6861a41433db37ba28905d96
treyzania <Treyzania@gmail.com>
2016-07-09 01:17:48 -0400

    platform-bukkit: Set up service registration following the new conventions and made the host plugin a proper module.

--- platform-bukkit/src/main/java/io/neocore/bukkit/NeocoreBukkitPlugin.java
+++ platform-bukkit/src/main/java/io/neocore/bukkit/NeocoreBukkitPlugin.java
@@ -1,19 +1,18 @@
 package io.neocore.bukkit;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.neocore.Neocore;
+import io.neocore.NeocoreImpl;
 import io.neocore.bukkit.events.ChatEventForwarder;
 import io.neocore.bukkit.events.EventForwarder;
 import io.neocore.bukkit.providers.BukkitBroadcastService;
 import io.neocore.bukkit.providers.BukkitChatService;
 import io.neocore.bukkit.providers.BukkitLoginService;
 import io.neocore.host.HostPlugin;
-import io.neocore.host.HostServiceProvider;
+import io.neocore.host.HostService;
 
 public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 	
@@ -31,6 +30,9 @@ public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 		
 		inst = this;
 		
+		// Initialize Neocore.
+		Neocore neo = new NeocoreImpl(this);
+		
 		// Support classes
 		this.chatForwarder = new ChatEventForwarder();
 		this.forwarders.add(this.chatForwarder);
@@ -41,6 +43,9 @@ public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 		this.loginService = new BukkitLoginService();
 		
 		// TODO Register services properly with Neocore.
+		neo.registerServiceProvider(HostService.BROADCAST, this.broadcastService, this);
+		neo.registerServiceProvider(HostService.CHAT, this.chatService, this);
+		neo.registerServiceProvider(HostService.LOGIN, this.loginService, this);
 		
 		// Event registration
 		for (EventForwarder fwdr : this.forwarders) {
@@ -68,8 +73,8 @@ public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 	}
 	
 	@Override
-	public List<HostServiceProvider> getServices() {
-		return Arrays.asList(this.broadcastService, this.chatService, this.loginService);
+	public String getVersion() {
+		return "${project.version}";
 	}
 	
 }
