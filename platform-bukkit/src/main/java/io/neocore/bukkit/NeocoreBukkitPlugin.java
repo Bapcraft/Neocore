4b3825c71ad8d61cfeea0de871fd70c3bb686df7
treyzania <Treyzania@gmail.com>
2016-07-24 19:57:43 -0400

    platform-bukkit: Moved announcement code into common.

--- platform-bukkit/src/main/java/io/neocore/bukkit/NeocoreBukkitPlugin.java
+++ platform-bukkit/src/main/java/io/neocore/bukkit/NeocoreBukkitPlugin.java
@@ -3,9 +3,6 @@ package io.neocore.bukkit;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,13 +10,8 @@ import io.neocore.NeocoreImpl;
 import io.neocore.api.Neocore;
 import io.neocore.api.NeocoreAPI;
 import io.neocore.api.NeocoreInstaller;
-import io.neocore.api.RegisteredService;
-import io.neocore.api.ServiceType;
-import io.neocore.api.database.DatabaseController;
-import io.neocore.api.database.DatabaseManager;
 import io.neocore.api.host.HostPlugin;
 import io.neocore.api.host.HostService;
-import io.neocore.api.module.Module;
 import io.neocore.bukkit.events.ChatEventForwarder;
 import io.neocore.bukkit.events.EventForwarder;
 import io.neocore.bukkit.providers.BukkitBroadcastService;
@@ -65,7 +57,7 @@ public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 			Bukkit.getPluginManager().registerEvents(fwdr, this);
 		}
 		
-		this.announceCompletion();
+		NeocoreAPI.announceCompletion();
 		
 	}
 	
@@ -95,50 +87,6 @@ public class NeocoreBukkitPlugin extends JavaPlugin implements HostPlugin {
 	@Override
 	public File getMicromoduleDirectory() {
 		return new File("micromodules");
-	}
-	
-	private void announceCompletion() {
-		
-		Logger log = Bukkit.getLogger();
-		log.info("=== Neocore Initialization Complete ===");
-		
-		Neocore neo = NeocoreAPI.getAgent();
-		
-		// Display host info
-		log.info("Host Class: " + neo.getHost().getClass().getName());
-		log.info("Host Version: " + neo.getHost().getVersion());
-		
-		// Display module info
-		log.info("Registered Modules:");
-		Set<Module> mods = neo.getModuleManager().getModules();
-		for (Module m : mods) {
-			log.info(String.format(" - %s v%s", m.getName(), m.getVersion()));
-		}
-		
-		// Display database engines
-		log.info("Database Controllers:");
-		DatabaseManager dbm = neo.getDatabaseManager();
-		for (Class<? extends DatabaseController> dbc : dbm.getControllers()) {
-			log.info(" - " + dbc.getSimpleName());
-		}
-		
-		// Display service info
-		log.info("Registerd Service Providers:");
-		List<RegisteredService> servs = neo.getServiceManager().getServices();
-		for (RegisteredService rs : servs) {
-			ServiceType st = rs.getType();
-			log.info(String.format(" - %s: %s %s (%s)", st.getName(), rs.getModule().getName(), rs.getServiceProvider().getClass().getSimpleName(), st.getClass().getSimpleName()));
-		}
-		
-		// List off all the other services that need to be dealt with eventually
-		log.info("Unprovisioned Services:");
-		List<ServiceType> types = neo.getServiceManager().getUnprovidedServices();
-		for (ServiceType st : types) {
-			log.info(String.format(" - %s (%s)", st.getName(), st.getClass().getSimpleName()));
-		}
-		
-		log.info("=======================================");
-		
 	}
 	
 }
