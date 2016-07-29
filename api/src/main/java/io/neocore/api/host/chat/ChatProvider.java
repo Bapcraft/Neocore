4b0f255f7fa16162c8ccd2228a3fdcd621c12931
treyzania <treyzania@gmail.com>
2016-07-29 01:22:45 -0400

    api: Added the method to ChatProvider to allow ChattablePlayer instances to be obtained at all.:P

--- api/src/main/java/io/neocore/api/host/chat/ChatProvider.java
+++ api/src/main/java/io/neocore/api/host/chat/ChatProvider.java
@@ -1,8 +1,12 @@
 package io.neocore.api.host.chat;
 
+import java.util.UUID;
+
 import io.neocore.api.host.HostServiceProvider;
 
 public interface ChatProvider extends HostServiceProvider {
+	
+	public ChattablePlayer getPlayer(UUID uuid);
 	
 	public void setChatAcceptor(ChatAcceptor acceptor);
 	public ChatAcceptor getChatAcceptor();
