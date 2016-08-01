57def83a4d4dee8c15dc4b28bbc26e493acaf5f5
treyzania <treyzania@gmail.com>
2016-08-01 01:40:00 -0400

    api: Made a few event interfaces inherit from Event properly.

--- api/src/main/java/io/neocore/api/event/PlayerEvent.java
+++ api/src/main/java/io/neocore/api/event/PlayerEvent.java
@@ -2,7 +2,7 @@ package io.neocore.api.event;
 
 import io.neocore.api.player.NeoPlayer;
 
-public interface PlayerEvent {
+public interface PlayerEvent extends Event {
 	
 	public NeoPlayer getPlayer();
 	
--- api/src/main/java/io/neocore/api/host/login/InitialLoginEvent.java
+++ api/src/main/java/io/neocore/api/host/login/InitialLoginEvent.java
@@ -3,12 +3,14 @@ package io.neocore.api.host.login;
 import java.net.InetAddress;
 import java.util.UUID;
 
+import io.neocore.api.event.Event;
+
 /**
  * Very tiny, lightweight class for events when players first connect.
  * 
  * @author treyzania
  */
-public interface InitialLoginEvent {
+public interface InitialLoginEvent extends Event {
 	
 	public UUID getPlayerUniqueId();
 	public InetAddress getAddress();
