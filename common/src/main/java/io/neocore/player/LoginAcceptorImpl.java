3359b046f2d00c9db229b7cfc83c17cf1cc87715
treyzania <Treyzania@gmail.com>
2016-08-01 01:39:20 -0400

    common: Hooked up the login acceptor to the event busses.

--- common/src/main/java/io/neocore/player/LoginAcceptorImpl.java
+++ common/src/main/java/io/neocore/player/LoginAcceptorImpl.java
@@ -5,40 +5,47 @@ import io.neocore.api.host.login.InitialLoginEvent;
 import io.neocore.api.host.login.LoginAcceptor;
 import io.neocore.api.host.login.PostLoginEvent;
 import io.neocore.api.player.NeoPlayer;
+import io.neocore.event.CommonEventManager;
 
 public class LoginAcceptorImpl implements LoginAcceptor {
 	
-	private CommonPlayerManager manager;
+	private CommonPlayerManager players;
+	private CommonEventManager events;
 	
-	public LoginAcceptorImpl(CommonPlayerManager man) {
-		this.manager = man;
+	public LoginAcceptorImpl(CommonPlayerManager players, CommonEventManager events) {
+		
+		this.players = players;
+		this.events = events;
+		
 	}
 	
 	@Override
 	public void onInitialLoginEvent(InitialLoginEvent event) {
 		
 		// Load the player data.
-		NeoPlayer np = this.manager.assemblePlayer(event.getPlayerUniqueId());
+		NeoPlayer np = this.players.assemblePlayer(event.getPlayerUniqueId());
 		
-		// TODO Forward to event bus
+		// Broadcast the event.
+		this.events.broadcast(event);
 		
 		// Unload the player if necessary.
-		if (!event.isPermitted()) this.manager.unloadPlayer(np);
+		if (!event.isPermitted()) this.players.unloadPlayer(np);
 		
 	}
 
 	@Override
 	public void onPostLoginEvent(PostLoginEvent event) {
-		// TODO Forward to event bus
+		this.events.broadcast(event);
 	}
 
 	@Override
 	public void onDisconnectEvent(DisconnectEvent event) {
 		
-		// TODO Forward to event bus.
+		// Broadcast the event straightaway.
+		this.events.broadcast(event);
 		
 		// Unload once we're sure everyone is done using it.
-		this.manager.unloadPlayer(event.getPlayer());
+		this.players.unloadPlayer(event.getPlayer());
 		
 	}
 
