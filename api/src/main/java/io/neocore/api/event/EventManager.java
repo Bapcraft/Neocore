dcb57ee430cff90f3f2351f4ea2c524379293e96
treyzania <treyzania@gmail.com>
2016-08-01 01:44:10 -0400

    api: Added utility method to set up the event manager with all the Neocore events.

--- api/src/main/java/io/neocore/api/event/EventManager.java
+++ api/src/main/java/io/neocore/api/event/EventManager.java
@@ -1,8 +1,29 @@
 package io.neocore.api.event;
 
+import io.neocore.api.host.chat.ChatEvent;
+import io.neocore.api.host.login.DisconnectEvent;
+import io.neocore.api.host.login.InitialLoginEvent;
+import io.neocore.api.host.login.PostLoginEvent;
+import io.neocore.api.host.proxy.DownstreamTransferEvent;
+
 public abstract class EventManager {
 	
 	public abstract <T extends Event> EventBus<T> registerEventType(Class<T> event);
 	public abstract <T extends Event> void broadcast(T event);
+	
+	public static void setupNeocoreEvents(EventManager man) {
+		
+		// Connection mediation stuff
+		man.registerEventType(InitialLoginEvent.class);
+		man.registerEventType(PostLoginEvent.class);
+		man.registerEventType(DisconnectEvent.class);
+		
+		// Proxy
+		man.registerEventType(DownstreamTransferEvent.class);
+		
+		// Chat
+		man.registerEventType(ChatEvent.class);
+		
+	}
 	
 }
