caaa212f5712821dfa326e6a9f49d7ba01289084
treyzania <treyzania@gmail.com>
2016-08-01 02:02:03 -0400

    api: Added definition to register listeners directly in the EventManager.

--- api/src/main/java/io/neocore/api/event/EventManager.java
+++ api/src/main/java/io/neocore/api/event/EventManager.java
@@ -1,13 +1,17 @@
 package io.neocore.api.event;
 
+import java.util.function.Consumer;
+
 import io.neocore.api.host.chat.ChatEvent;
 import io.neocore.api.host.login.DisconnectEvent;
 import io.neocore.api.host.login.InitialLoginEvent;
 import io.neocore.api.host.login.PostLoginEvent;
 import io.neocore.api.host.proxy.DownstreamTransferEvent;
+import io.neocore.api.module.Module;
 
 public abstract class EventManager {
 	
+	public abstract <T extends Event> EventBus<T> registerListener(Module mod, Class<T> clazz, Consumer<T> listener, int priority);
 	public abstract <T extends Event> EventBus<T> registerEventType(Class<T> event);
 	public abstract <T extends Event> void broadcast(T event);
 	
