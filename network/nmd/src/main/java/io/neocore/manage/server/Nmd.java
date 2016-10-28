a411a8e2640d3caa5b4697cccc1d413541ec1133
treyzania <treyzania@gmail.com>
2016-10-28 02:06:04 -0400

    network: Added logging capability.

--- network/nmd/src/main/java/io/neocore/manage/server/Nmd.java
+++ network/nmd/src/main/java/io/neocore/manage/server/Nmd.java
@@ -2,6 +2,14 @@ package io.neocore.manage.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.UUID;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.typesafe.config.Config;
@@ -11,6 +19,9 @@ public class Nmd {
 	public static Logger logger = Logger.getLogger("NMD");
 	public static Nmd instance;
 	
+	private long initTime, startTime;
+	private UUID daemonExecId;
+	
 	private Logger log;
 	private DaemonServer server;
 	
@@ -18,28 +29,94 @@ public class Nmd {
 	
 	public Nmd(Logger log, Config conf) {
 		
-		this.log = log;
-		this.scheduler = new Scheduler(this.log);
+		this.initTime = System.currentTimeMillis();
+		this.daemonExecId = UUID.randomUUID();
 		
+		this.log = log;
+		
+		// Set up the logger.
 		try {
 			
-			ServerSocket socket = new ServerSocket();
+			this.log.setUseParentHandlers(false);
+			Formatter formatter = new NmdLogFormatter();
 			
-			this.server = new DaemonServer(this.scheduler, socket, 1);
+			FileHandler fileHandler = new FileHandler(this.getLogFileName(), false);
+			fileHandler.setFormatter(formatter);
+			this.log.addHandler(fileHandler);
 			
+			ConsoleHandler consoleHandler = new ConsoleHandler();
+			consoleHandler.setFormatter(formatter);
+			this.log.addHandler(consoleHandler);
+			
+		} catch (SecurityException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		// Tell user we're doing something.
+		this.log.info("NMD vDEV");
+		this.log.info("Contribute on GitHub: https://github.com/bapcraft");
+		this.log.info("Daemon Execution ID: " + this.getExecutionId());
+		this.log.info("Initializing...");
+		
+		// Scheduler.
+		this.scheduler = new Scheduler(this.log);
+		
+		// Now set up the socket and server itself.
+		try {
+			
+			ServerSocket socket = new ServerSocket(10000);
+			this.server = new DaemonServer(this.scheduler, socket, 1);
+			
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, "Could not initialize socket!", e);
+		}
+		
+		this.log.info("Daemon initialized!");
+		
+	}
+	
+	public long getInitTime() {
+		return this.initTime;
+	}
+	
+	public long getStartTime() {
+		return this.startTime;
+	}
+	
+	public boolean isStarted() {
+		return this.getStartTime() > 0L;
+	}
+	
+	public String getLogFileName() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
+		return String.format("%s.log", sdf.format(new Date(this.getInitTime())));
+		
+	}
+	
+	public UUID getExecutionId() {
+		return this.daemonExecId;
 	}
 	
 	public DaemonServer getServer() {
 		return this.server;
 	}
 	
-	public void start() {
+	public synchronized void start() {
+		
+		if (this.isStarted()) throw new IllegalStateException("NMD already started!");
+		
+		this.log.info("Starting listening threads...");
+		this.startTime = System.currentTimeMillis();
 		this.getServer().startListening();
+		
+		long millis = this.getStartTime() - this.getInitTime();
+		this.log.info("Done! (" + (millis / 1000) + "s, " + (millis % 1000) + " ms)");
+		
 	}
 	
 	public static void main(String[] args) {
@@ -47,6 +124,7 @@ public class Nmd {
 		Config conf = null;
 		
 		Nmd nmd = new Nmd(logger, conf);
+		
 		nmd.start();
 		
 	}
