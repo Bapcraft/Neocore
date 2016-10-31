158a90b0f66fe98aecb46dca89ecea56460fa5d2
treyzania <treyzania@gmail.com>
2016-10-31 09:30:57 -0400

    api: Actually added a way to get extensions from and attach extensions onto players.

--- api/src/main/java/io/neocore/api/database/player/DatabasePlayer.java
+++ api/src/main/java/io/neocore/api/database/player/DatabasePlayer.java
@@ -8,6 +8,7 @@ import io.neocore.api.database.ban.BanList;
 import io.neocore.api.database.ban.BanService;
 import io.neocore.api.eco.Account;
 import io.neocore.api.player.PlayerIdentity;
+import io.neocore.api.player.extension.Extension;
 import io.neocore.api.player.group.Group;
 import io.neocore.api.player.group.GroupMembership;
 
@@ -49,6 +50,21 @@ public interface DatabasePlayer extends PlayerIdentity, PersistentPlayerIdentity
 	 * @return The account object representing their store of the currency.
 	 */
 	public Account getAccount(String currency);
+	
+	/**
+	 * Gets the extension of the given name/type.
+	 * 
+	 * @param name The name of the extension type.
+	 * @return The extension we have on the player, or <code>null</code> if we don't have it.
+	 */
+	public Extension getExtension(String name);
+	
+	/**
+	 * Attaches the extension onto the player, replacing any other extensions of that type.
+	 * 
+	 * @param ext The extension to attach.
+	 */
+	public void putExtension(Extension ext);
 	
 	/**
 	 * Sets the record of the player's last known username.
