6abb62d92d29d967ba7f22b4c709a8773a557841
treyzania <treyzania@gmail.com>
2016-11-07 02:33:01 -0500

    api: Made the LoadAsync annotation persist to inherited classes.

--- api/src/main/java/io/neocore/api/LoadAsync.java
+++ api/src/main/java/io/neocore/api/LoadAsync.java
@@ -3,6 +3,7 @@ package io.neocore.api;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -14,6 +15,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RUNTIME)
 @Target(TYPE)
+@Inherited
 public @interface LoadAsync {
 
 }
