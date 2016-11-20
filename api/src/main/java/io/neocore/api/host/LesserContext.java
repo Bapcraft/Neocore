32257bd45886bc643a7ddfad46e904c3c1e0e4be
treyzania <treyzania@gmail.com>
2016-11-20 14:03:42 -0500

    api: Made LesserContext more "value objecty".

--- api/src/main/java/io/neocore/api/host/LesserContext.java
+++ api/src/main/java/io/neocore/api/host/LesserContext.java
@@ -18,4 +18,37 @@ public class LesserContext implements Context {
 		return this.name;
 	}
 	
+	@Override
+	public int hashCode() {
+		
+		// Yucky auto-generated code!
+		
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		// Yucky auto-generated code!
+		
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LesserContext other = (LesserContext) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+		
+	}
+	
 }
