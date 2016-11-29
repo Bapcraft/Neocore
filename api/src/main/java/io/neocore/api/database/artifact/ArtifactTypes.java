5812193dfd22918c4313377f71b76719ecb8580f
treyzania <treyzania@gmail.com>
2016-11-28 19:46:35 -0500

    api: Added a "config" artifact ID for the default broadcast prefix.

--- api/src/main/java/io/neocore/api/database/artifact/ArtifactTypes.java
+++ api/src/main/java/io/neocore/api/database/artifact/ArtifactTypes.java
@@ -3,13 +3,13 @@ package io.neocore.api.database.artifact;
 public class ArtifactTypes {
 	
 	// Default value.  Shouldn't really be used.
-	public static final String UNCLASSIFIED = "neocore.unclassified";
-	
-	// "Settings" values.
-	public static final String DATA_IDENTIFIER = "neocore.identifier";
+	public static final String UNCLASSIFIED = "UNCLASSIFIED";
 	
 	// Administration purposes for helping run servers.
 	public static final String ADMIN_EVIDENCE = "neocore.admin.evidence";
 	public static final String ADMIN_WARNING = "neocore.admin.warning";
+	
+	// Specific "config" values.
+	public static final String BCAST_DEFAULT_PREFIX = "neocore.broadcast.DefaultPrefix";
 	
 }
