127e507dabea4cb5e860506176e998c23a996285
treyzania <treyzania@gmail.com>
2017-01-13 01:26:39 -0500

    platform-bungeecord: Changed default threading model to AUTO_DEFERRED.

--- platform-bungeecord/src/main/java/io/neocore/bungee/BungeeNeocoreConfig.java
+++ platform-bungeecord/src/main/java/io/neocore/bungee/BungeeNeocoreConfig.java
@@ -27,7 +27,7 @@ public class BungeeNeocoreConfig implements NeocoreConfig {
 	private List<Context> contexts;
 
 	private static final String pIoModel_key = "server.playerThreadingModel";
-	private PlayerIoThreadingModel pIoModel = PlayerIoThreadingModel.AUTO;
+	private PlayerIoThreadingModel pIoModel = PlayerIoThreadingModel.AUTO_DEFERRED;
 	
 	public BungeeNeocoreConfig(Config conf) {
 		
